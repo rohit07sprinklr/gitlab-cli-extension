9041588a8fcd17ec68c4e7461f407bbfdecad32b
rohit kumar <rohit.kumar@sprinklr.com>
2022-06-06 23:51:06 +0530

    Merge conflict Addition

--- src/server/api.js
+++ src/server/api.js
@@ -106,16 +106,28 @@ app.get("/merge", async function (req, res) {
     await git(path).checkout(target);
     await git(path).raw("reset", "--hard", `origin/${target}`);
 
-    await git(path).raw("merge", "--no-ff", source, "--no-edit");
+    const result = await git(path).raw("merge","--no-commit","--no-ff", source);
+    const conflictStatus = (result.split('\n'))[1];
+    if(conflictStatus == undefined){
+      console.log('No Conflict detected!');
 
-    console.log(`merged, pushing ${target}`);
-    res.write(`merged, pushing ${target}`);
-    await git(path).push("origin", target);
+      res.write(`No Conflict detected: Commiting Changes`);
+      await git(path).raw("commit", "-m","Merged");
 
-    console.log(`pushed ${target}`);
-    res.write(`pushed ${target}`);
-    await wait(2000);
-    console.log("end merge successfully");
+      console.log(`merged, pushing ${target}`);
+      res.write(`merged, pushing ${target}`);
+      await git(path).push("origin", target);
+
+      console.log(`pushed ${target}`);
+      res.write(`pushed ${target}`);
+      await wait(2000);
+      console.log("end merge successfully");
+    }
+    else if(conflictStatus.startsWith("CONFLICT")){
+      console.log('Conflict Encountered: Aborting');
+      await git(path).raw("merge", "--abort");
+      throw new Error("Conflict Encountered: Merge Aborted!");
+    }
     res.end();
   } catch (e) {
     res.write(`error: ${e.toString()}`);
@@ -123,59 +135,6 @@ app.get("/merge", async function (req, res) {
     res.write(`ERROR`);
     console.error(e);
     console.log("end merge failure");
-    res.end();
-  }
-  markIdle();
-});
-
-app.get("/rebase", async function (req, res) {
-  res.writeHead(200, {
-    "Content-Type": "text/plain",
-    "Transfer-Encoding": "chunked",
-    "access-control-allow-origin": "*",
-  });
-  if (cliStatus !== "IDLE") {
-    res.write(`CLI Busy`);
-    await wait(100);
-    res.write(`ERROR`);
-    res.end();
-    return;
-  }
-  markWIP();
-
-  try {
-    const { source, target, location } = req.query;
-    const path = config.repos.find((repo) =>
-      location.startsWith(repo.url)
-    ).path;
-
-    console.log("start rebase");
-    console.log(`fetching ${source}`);
-    res.write(`fetching ${source}`);
-    await git(path).fetch("origin", source);
-
-    await git(path).checkout(source);
-    await git(path).raw("reset", "--hard", `origin/${source}`);
-
-    console.log(`pulling ${target}`);
-    res.write(`pulling ${target}`);
-    await git(path).pull("origin", target, { "--rebase": null });
-
-    console.log(`rebased, force pushing ${source}`);
-    res.write(`rebased, force pushing ${source}`);
-    await git(path).push("origin", source, { "-f": null });
-
-    console.log(`pushed ${source}`);
-    res.write(`pushed ${source}`);
-    await wait(2000);
-    console.log("end rebase successfully");
-    res.end();
-  } catch (e) {
-    res.write(`error: ${e.toString()}`);
-    await wait(100);
-    res.write(`ERROR`);
-    console.error(e);
-    console.log("end rebase failure");
     res.end();
   }
   markIdle();
