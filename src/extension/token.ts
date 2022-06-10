9bbdea81495c2e19b191e24f53fdc41a54b7da80
rohit kumar <rohit.kumar@sprinklr.com>
2022-06-10 09:13:09 +0530

    Code Fixes

--- src/extension/token.ts
+++ src/extension/token.ts
@@ -1,25 +1,33 @@
-const main = () => {
-    const windowURL = new URL(window.location);
-    if(windowURL.searchParams.toString()=='create_gitlabcli_token=true'){
-        const inputArray = Array.from(document.querySelectorAll('input'))
-
-        const scopes = inputArray.filter(input => input.name === 'personal_access_token[scopes][]')
+function fillTokenScope(inputArray){
+    const scopes = inputArray.filter(input => input.name === 'personal_access_token[scopes][]')
         scopes.forEach((element)=>{
             element.setAttribute("checked",true);
-        })
-        
-        const tokenName = inputArray.find(input => input.name === 'personal_access_token[name]');
+    })
+}
+function fillTokenName(inputArray){
+    const tokenName = inputArray.find(input => input.name === 'personal_access_token[name]');
         tokenName.value = 'GitlabCLIToken';
-
-        const tokenExpireDate = inputArray.find(input => input.name === 'personal_access_token[expires_at]');
+}
+function fillTokenDate(inputArray){
+    const tokenExpireDate = inputArray.find(input => input.name === 'personal_access_token[expires_at]');
         const today = new Date();
         const date = `${today.getFullYear()+1}-${("0" + (today.getMonth()+1)).slice(-2)}-${("0" + (today.getDate()+1)).slice(-2)}`;
         tokenExpireDate.value = date;
-
-        const submitButton = inputArray.find(input => input.name === 'commit');
+}
+function submitToken(inputArray){
+    const submitButton = inputArray.find(input => input.name === 'commit');
         submitButton.classList.remove('disabled');
         submitButton.removeAttribute("disabled");
         submitButton.click();
+}
+const main = () => {
+    const windowURL = new URL(window.location);
+    if(windowURL.searchParams.toString()=='create_gitlabcli_token=true'){
+        const inputArray = Array.from(document.querySelectorAll('input'));
+        fillTokenScope(inputArray);
+        fillTokenName(inputArray)
+        fillTokenDate(inputArray);
+        submitToken(inputArray);   
     }
     else{
         const token = document.querySelector(".created-personal-access-token-container");
