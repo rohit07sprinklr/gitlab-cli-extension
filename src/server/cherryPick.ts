8f9f5dd22a9a9ff5d9329d61cafb15ee3d89f2d2
rohit kumar <rohit.kumar@sprinklr.com>
2022-06-20 20:32:22 +0530

    fixes reset hard on continue request

--- src/server/cherryPick.ts
+++ src/server/cherryPick.ts
@@ -9,34 +9,34 @@ async function cherryPickProcess(req, res) {
     const path = req.body.localPath;
     const commitBranch = req.body.commitBranch;
     const targetBranch = req.body.targetBranch;
+    const requestType = req.body.requestType;
     await wait(100);
-    try {
-      await git(path).listRemote([
-        "--heads",
-        "--exit-code",
-        "origin",
-        `${commitBranch}`,
-      ]);
-      //If found on remote repo
-      await git(path).fetch("origin", commitBranch);
-      await git(path).checkout(commitBranch);
-      await git(path).raw("reset", "--hard", `origin/${commitBranch}`);
-    } catch {
-      const anss = await git(path).branchLocal();
-      if (anss.all.includes(`${commitBranch}`)) {
-        //Not found on remote repo Found in local repo
+    if(requestType === 'new'){
+      try {
+        await git(path).listRemote([
+          "--heads",
+          "--exit-code",
+          "origin",
+          `${commitBranch}`,
+        ]);
+        //If found on remote repo
+        await git(path).fetch("origin", commitBranch);
         await git(path).checkout(commitBranch);
-      }
-      //Niether in local nor remote repo checkout new branch from target branch
-      else {
+        await git(path).raw("reset", "--hard", `origin/${commitBranch}`);
+      }catch{
+        //Niether in local nor remote repo checkout new branch from target branch
         await git(path).checkoutBranch(`${commitBranch}`, `${targetBranch}`);
       }
+    }else if(requestType === "continue"){
+      await git(path).checkout(commitBranch);
     }
     const gitLogs = req.body.commits;
-    let currentCommitIndex = 0;
+    let completedCommits = 0;
+    let currentCommitSHA;
     try {
       for (const gitLog of gitLogs) {
-        currentCommitIndex += 1;
+        completedCommits += 1;
+        currentCommitSHA = gitLog.commitSHA;
         if (gitLog.commitScope == false) {
           continue;
         }
@@ -56,9 +56,10 @@ async function cherryPickProcess(req, res) {
     } catch (e) {
       await git(path).raw("reset", "--hard");
       console.log("Failed");
-      res.write(`Cherry-pick paused - ${e.toString()}`);
+      res.write(`<strong> Automatic Cherry-pick ${currentCommitSHA} Failed: You can still cherry-pick this commit manually. 
+      Press Continue to proceed or Stop to End</strong> <br> <hr> ${e.toString()}`);
       await wait(100);
-      res.write(`Paused {${currentCommitIndex}}`);
+      res.write(`Paused {${completedCommits}}`);
       res.end();
       console.log(e);
       return;
